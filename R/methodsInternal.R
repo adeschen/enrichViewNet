33e4f1367151734b3305cdea92d44043afa7defc
Astrid Deschenes <adeschen@hotmail.com>
2024-04-04 19:25:54 -0400

    Update doc for extractNodesAndEdgesInformation() function

--- R/methodsInternal.R
+++ R/methodsInternal.R
@@ -276,11 +276,11 @@ removeRootTerm <- function(gostResult) {
 #'
 #' @return \code{list} containing 2 entries:
 #' \itemize{
-#' \item{"geneNodes"}{a \code{data.frame} containing the information about
-#' the nodes present in the network. The nodes are genes.}
-#' \item{"termNodes"}{a \code{data.frame} containing the information about
-#' the nodes present in the network. The nodes are terms.}
-#' \item{"edges"}{a \code{data.frame} containing the information about
+#' \item{\code{"geneNodes"}: a \code{data.frame} containing the information 
+#' about the nodes present in the network. The nodes are genes.}
+#' \item{\code{"termNodes"}: a \code{data.frame} containing the information 
+#' about the nodes present in the network. The nodes are terms.}
+#' \item{\code{"edges"}: a \code{data.frame} containing the information about
 #' the edges present in the network. The edges connect one gene to one term.}
 #' }
 #'
