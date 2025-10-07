c587a3e9a14dfe501085180ee99194f53e24ee4a
adeschen <adeschen@hotmail.com>
2025-10-07 16:45:19 -0400

    Adding unit tests

--- tests/testthat/test-methodsEmap.R
+++ tests/testthat/test-methodsEmap.R
@@ -443,6 +443,127 @@ test_that("createEnrichMapMultiBasic() must return error when number in queryLis
 })
 
 
+### Tests createEnrichMapMultiBasicAsIgraph() results
+
+context("createEnrichMapMultiBasicAsIgraph() results")
+
+test_that("createEnrichMapMultiBasicAsIgraph() must return error when gostObjectList is a number", {
+    
+    error_message <- paste0("The gostObjectList object should be a list ", 
+        "of enrichment objects. At least 2 enrichment objects are required.")
+    
+    expect_error(createEnrichMapMultiBasicAsIgraph(gostObjectList=33, 
+        queryList=c("TEST", "Test2"),  source="GO:CC", termIDs=NULL, 
+        removeRoot=TRUE, showCategory=30, similarityCutOff=0.2), error_message)
+})
+
+test_that("createEnrichMapMultiBasicAsIgraph() must return error when gostObjectList has only one element", {
+    
+    gostTerm <- demoGOST
+    
+    error_message <- paste0("The gostObjectList object should be a list ", 
+        "of enrichment objects. At least 2 enrichment objects are required.")
+    
+    expect_error(createEnrichMapMultiBasicAsIgraph(gostObjectList=list(gostTerm), 
+        queryList=list("TEST"), source="GO:CC", termIDs=NULL, removeRoot=TRUE, 
+        showCategory=30, similarityCutOff=0.1), error_message)
+})
+
+test_that("createEnrichMapMultiBasicAsIgraph() must return error when queryList is a number", {
+    
+    gostTerm <- demoGOST
+    
+    error_message <- paste0("The queryList object should be a list of query ", 
+        "names. At least 2 query names are required. The number of query ", 
+        "names should correspond to the number of enrichment objects.")
+    
+    expect_error(createEnrichMapMultiBasicAsIgraph(gostObjectList=list(gostTerm, gostTerm), 
+        queryList=33, source="GO:CC", termIDs=NULL, removeRoot=TRUE, 
+        showCategory=30, similarityCutOff=0.2), error_message)
+})
+
+test_that("createEnrichMapMultiBasicAsIgraph() must return error when queryList is longer than gostObjectList", {
+    
+    gostTerm <- demoGOST
+    
+    error_message <- paste0("The queryList object should be a list of query ", 
+        "names. At least 2 query names are required. The number of query ", 
+        "names should correspond to the number of enrichment objects.")
+    
+    expect_error(createEnrichMapMultiBasicAsIgraph(gostObjectList=list(gostTerm, gostTerm), 
+        queryList=list("TEST", "TEST2", "TEST3"), source="GO:CC", termIDs=NULL, 
+        removeRoot=TRUE, showCategory=30, similarityCutOff=0.1), error_message)
+})
+
+test_that("createEnrichMapMultiBasicAsIgraph() must return error when one query in queryList is not in gostObject", {
+    
+    gostTerm <- demoGOST
+    
+    error_message <- paste0("Each query name present in the ", 
+        "\'queryList\' parameter must be present in the associated ", 
+        "enrichment object.")
+    
+    expect_error(createEnrichMapMultiBasicAsIgraph(gostObjectList=list(gostTerm, gostTerm), 
+        queryList=list("query_1", "TEST"), source="GO:CC", termIDs=NULL, 
+        removeRoot=TRUE, showCategory=30, similarityCutOff=0.2), 
+        error_message, fixed=TRUE)
+})
+
+test_that("createEnrichMapMultiBasicAsIgraph() must return error when one object in gostObjectList in a number", {
+    
+    gostTerm <- demoGOST
+    
+    error_message <- paste0("The gostObjectList should only contain a list ", 
+        "of enrichment results. Enrichment results are lists with meta ", 
+        "and result as entries corresponding to gprofiler2 ", 
+        "enrichment output.")
+    
+    expect_error(createEnrichMapMultiBasicAsIgraph(gostObjectList=list(gostTerm, 33), 
+        queryList=list("query_1", "query_1"), source="GO:CC", termIDs=NULL, 
+        removeRoot=TRUE, showCategory=30, similarityCutOff=0.2), 
+        error_message, fixed=TRUE)
+})
+
+test_that("createEnrichMapMultiBasicAsIgraph() must return error when no enriched term for selected source", {
+    
+    gostTerm <- demoGOST
+    gostTerm$result <- gostTerm$result[which(gostTerm$result$source != "WP"), ]
+    
+    error_message <- paste0("There is no enriched term for the selected ", 
+        "source \'WP\'.")   
+    
+    expect_error(createEnrichMapMultiBasicAsIgraph(gostObjectList=list(gostTerm, gostTerm), 
+        queryList=list("query_1", "query_1"), source="WP", termIDs=NULL, 
+        removeRoot=TRUE, similarityCutOff=0.2), error_message, fixed=TRUE)
+})
+
+test_that("createEnrichMapMultiBasicAsIgraph() must return error when number in queryList", {
+    
+    gostTerm <- demoGOST
+    
+    error_message <- paste0("The queryList object should only contain a list ", 
+                            "of query names in character strings.")
+    
+    expect_error(createEnrichMapMultiBasicAsIgraph(gostObjectList=list(gostTerm, gostTerm), 
+        queryList=list("query_1", 33), source="WP", termIDs=NULL, 
+        removeRoot=TRUE, showCategory=30, similarityCutOff=0.1), 
+        error_message, fixed=TRUE)
+})
+
+test_that("createEnrichMapMultiBasicAsIgraph() must return error when number in queryList", {
+    
+    gostTerm <- demoGOST
+    
+    error_message <- paste0("A vector of terms should be given through the ",
+        "\'termIDs\' parameter when source is \'TERM_ID\'.")
+    
+    expect_error(createEnrichMapMultiBasicAsIgraph(gostObjectList=list(gostTerm, gostTerm), 
+        queryList=list("query_1", "query_1"), source="TERM_ID", termIDs=NULL, 
+        removeRoot=TRUE, showCategory=30, similarityCutOff=0.2), 
+        error_message, fixed=TRUE)
+})
+
+
 ### Tests createEnrichMapMultiComplex() results
 
 context("createEnrichMapMultiComplex() results")
